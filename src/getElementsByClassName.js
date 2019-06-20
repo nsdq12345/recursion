19822d3faa47698038f1d725b2bc7113e691347e
Jian Yeung <yeungjian@gmail.com>
2019-06-19 21:05:04 -0700

    finished first version of getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,30 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var elementsWithClass = [];
+  lookthroughElements(document.body);
+
+
+  function lookthroughElements(element) {
+
+  	if (element.className != undefined) {
+  		var classList = element.className.split(/\s+/);
+
+
+	  	for (var i = 0; i < classList.length; i++) {
+	  		if (className == classList[i]) {
+	  			elementsWithClass.push(element);
+	  		}
+	  	}
+	}
+
+
+	if (element.children != undefined) {
+	  	for (var i = 0; i < element.children.length; i++) { 
+	  		lookthroughElements(element.children[i]);
+	  	}
+	}
+  }
+
+  return elementsWithClass;
 };
